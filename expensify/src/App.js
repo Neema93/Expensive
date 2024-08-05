39654e97b523ac5d93e30b26e6cd11e75815d115
Neema <patelneema93@gmail.com>
2024-08-05 14:08:08 -0400

    create some route

--- expensify/src/App.js
+++ expensify/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+const ExpenseDashbordPage = () => (
+  <div>
+    This is Dashbord.
+  </div>
+);
+const CreatePage = () => (
+  <div>
+    This is CreateExpensify.
+  </div>
+);
+const EditPage = () => (
+  <div>
+    This is EditExpensify.
+  </div>
+);
+const HelpPage = () => (
+  <div>
+    This is Help.
+  </div>
+);
+const NotFound = () => (
+  <div>
+    404.
+  </div>
+);
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expensify</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={ExpenseDashbordPage} />
+        <Route path="/create" Component={CreatePage} /> 
+        <Route path="/edit" Component={EditPage} /> 
+        <Route path="/help" Component={HelpPage} />
+        <Route  path="*" Component={NotFound} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
