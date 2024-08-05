889ac73832bdb6059994de5e1f99c90a6264e139
Neema <patelneema93@gmail.com>
2024-08-05 14:52:09 -0400

    appRouter create

---
+++ expensify/src/routers/AppRouter.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+// import './App.css';
+//import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes, Link, NavLink} from 'react-router-dom';
+const ExpenseDashbordPage = () => (
+    <div>
+      This is Dashbord.
+    </div>
+  );
+  const CreatePage = () => (
+    <div>
+      This is CreateExpensify.
+    </div>
+  );
+  const EditPage = () => (
+    <div>
+      This is EditExpensify.
+    </div>
+  );
+  const HelpPage = () => (
+    <div>
+      This is Help.
+    </div>
+  );
+  const NotFound = () => (
+    <div>
+      404 - <Link to='/'>Go Home</Link>.
+    </div>
+  );
+  const Header = () => (
+    <header>
+       <h1>Expensify</h1>
+       <NavLink to='/' className={({ isActive}) => isActive ? "active" : "" } >Home</NavLink>&nbsp;&nbsp;
+       <NavLink to='/create' className={({ isActive}) => isActive ? "active" : "" }>Create</NavLink>&nbsp;&nbsp;
+       <NavLink to='/edit' className={({ isActive}) => isActive ? "active" : "" }>Edit</NavLink>&nbsp;&nbsp;
+       <NavLink to='/help'className={({ isActive}) => isActive ? "active" : "" }>Help</NavLink>
+    </header>
+  )
+  function AppRouter() {
+  
+    return (
+      <div className="App">
+       
+        <BrowserRouter>
+        <div>
+          <Header />
+  
+        </div>
+        <Routes>
+          <Route path="/" Component={ExpenseDashbordPage} />
+          <Route path="/create" Component={CreatePage} /> 
+          <Route path="/edit" Component={EditPage} /> 
+          <Route path="/help" Component={HelpPage} />
+          <Route  path="*" Component={NotFound} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  export default AppRouter;
