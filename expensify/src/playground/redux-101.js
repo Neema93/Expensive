f5701d7a1f3c5b4a57fab3da50e522b1cd2b6fb2
Neema <patelneema93@gmail.com>
2024-08-05 17:02:30 -0400

    create set count number

--- expensify/src/playground/redux-101.js
+++ expensify/src/playground/redux-101.js
@@ -11,6 +11,10 @@ const store = createStore((state = {count:0}, action) =>{
             return{ 
                 count: state.count - decrementBy
             };
+        case 'SET':
+            return{
+                count:action.count
+            }
         case 'RESET':
             return{
                 count:0
@@ -42,6 +46,10 @@ store.dispatch({
 store.dispatch({
     type: 'DECREMENT',
     decrementBy: 6
+})
+store.dispatch({
+    type: 'SET',
+    count: 101
 })
 
 export default store;
