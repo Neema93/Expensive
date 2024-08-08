845dcad40f0dbad2b12be1a5f8f5c1aacef9f360
Neema <patelneema93@gmail.com>
2024-08-08 14:24:10 -0400

    orgunizing redux

--- expensify/src/App.js
+++ expensify/src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 
 import './App.css';
 import AppRouter from './routers/AppRouter';
-import store from './playground/redux-101';
-import person from './playground/destructuring';
-import demoState from './playground/redux-expensify';
+import configurestore from './store/configurestore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+// import store from './playground/redux-101';
+// import person from './playground/destructuring';
+// import demoState from './playground/redux-expensify';
+
 function App(){
-  console.log(store.getState());
-  console.log(person);
-  console.log(demoState);
+  const store = configurestore();
+  store.dispatch(addExpense({description:'water bill'}))
+  store.dispatch(addExpense({description:'gas bill'}))
+  store.dispatch(setTextFilter('gas'))
+
+ const state = store.getState();
+ const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
+ console.log(visibleExpenses);
   return (
     <div className="App">
 
---
+++ expensify/src/store/configurestore.jsx
@@ -0,0 +1,12 @@
+import { legacy_createStore as createStore, combineReducers } from "redux";
+import expensesReducer from "../reducers/expenses";
+import filtersRedusers from "../reducers/filters"
+export default () => {
+    const store = createStore(
+        combineReducers({
+            expenses:expensesReducer,
+            filters:filtersRedusers
+        })
+    );
+    return store;
+}
