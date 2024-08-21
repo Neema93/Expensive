53e95f5a720af40b1d9f2bdbe4eed1050123043e
Neema <patelneema93@gmail.com>
2024-08-21 14:07:29 -0400

    create action expense test

--- expensify/src/tests/add.test.js
+++ expensify/src/tests/add.test.js
@@ -1,6 +1,15 @@
 const add = (a,b) => a + b;
+const generateGreeting = (name) => `Hello ${name}`;
 
 test('should add two numbers', () => {
     const result = add(3,4);
    expect(result).toBe(7);
+})
+test('should generate greeting from name', () => {
+    const name = generateGreeting('Neema');
+   expect(name).toBe(`Hello Neema`);
+})
+test('should generate greeting for no name', () => {
+    const name = generateGreeting('');
+   expect(name).toBe(`Hello `);
 })
