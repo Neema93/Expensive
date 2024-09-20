76e02b43f6a01563da32edfbed44c9a5da2cf7cc
Neema <patelneema93@gmail.com>
2024-09-20 16:53:36 -0400

    data get value

--- expensify/src/playground/redux-user.jsx
+++ expensify/src/playground/redux-user.jsx
@@ -1,13 +1,9 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers ,applyMiddleware} from "redux";
 import {v1 as uuid} from 'uuid';
-import { createAsyncThunk } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux';
+import {thunk} from 'redux-thunk';
 
-export const fetchUsers = createAsyncThunk("fetchTodos", async () => {
-    const res = await fetch(`https://localhost:9000/users`);
-    return res?.json();
- });
- console.log(fetchUsers());
+
+
 const addUser = ({userName = '', password = ''} = {}) =>({
     type: 'ADD_USER',
     user:{
@@ -26,20 +22,19 @@ const editUser = (id , update) =>({
     update
 
 })
-const loginsuccess = (userName ,password) => ({
-    type: 'LOGINSUCCESS',
-    userName,
-    password
-})
-
-const loginFail = () =>({
-    type: 'LOGINFAIL'
-
-})
-
-const logout = () =>({
-    type: 'LOGOUT'
-})
+ 
+const loginSuccess = (user) => ({
+    type: 'LOGIN_SUCCESS',
+    payload: user,
+  });
+const loginFailure = (error) => ({
+    type: 'LOGIN_FAILURE',
+    payload: error,
+  });
+  export const logout = () => ({
+    type: 'LOGOUT',
+  });
+  // Example of an asynchronous login action
 
 const userReduserDefaultState =[];
 const userReducer = (state =userReduserDefaultState , action) => {
@@ -63,54 +58,116 @@ const userReducer = (state =userReduserDefaultState , action) => {
             return state;
     }
 };
-const loginRedusersDefaultState = {
-      user:{}
-};
-const authRedusers =(state = loginRedusersDefaultState, action) =>
-{
-    switch(action.type){  
-        case 'LOGINSUCCESS':
-            return{
-               user:action.user
-            };
-        case 'LOGINFAIL':
-            return{
-               user:{}
-            };
-        case 'LOGOUT':  
-        return{
-            user:{}
-        }; 
-       
-        default:
-            return state;
+
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+    loading: false,
+    error: null,
+  };
+  
+  const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+   
+      case 'LOGIN_SUCCESS':
+        return {
+          ...state,
+          isAuthenticated: true,
+          user: action.payload,
+          loading: false,
+        };
+      case 'LOGIN_FAILURE':
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+      case 'LOGOUT':
+        return {
+          ...state,
+          isAuthenticated: false,
+          user: null,
+        };
+      default:
+        return state;
     }
-}
-// const getVisibleAuth = (user, {userName = '' , password= ''}) =>{
+  };
+  
+
+  
+// const getVisibleAuth = (user,isLogin, {userName = '' , password= ''}) =>{
 //     return user.filter((user) => {
 
 //         const userNameMatch = user.userName.toLowerCase().includes(userName.toLowerCase());
 //         const passwordMatch = user.password.toLowerCase().includes(password.toLowerCase());
-//         return userNameMatch && passwordMatch; 
+//         if(userNameMatch && passwordMatch) {
+//             return isLogin = true; 
+//         }else {
+//             isLogin = false;
+//         }
+     
 //     })
 // }
 const store = createStore(
     combineReducers({
-        user:userReducer
-    
+        user:userReducer,
+        auth:authReducer
+       
 
-    })
+    }),applyMiddleware(thunk)
 );
 store.subscribe(() => {
     const state = store.getState();
-    // const VisibleUser = getVisibleUser(state.user);
+    // const VisibleUser = getVisibleAuth(state.user);
     console.log(state);
+    // console.log(VisibleUser);
 })
-const userOne = store.dispatch(addUser({userName:'Neema',password:'123'}))
-const userTwo= store.dispatch(addUser({userName:'heeya',password:'123'}))
-store.dispatch(removeUser({id: userOne.user.id }))
-store.dispatch(editUser(userTwo.user.id, {password: '567'}))
-store.dispatch(loginsuccess('Neema','123'));
+
+
+// const userOne = store.dispatch(addUser({userName:'Neema',password:'123'}))
+// const userTwo= store.dispatch(addUser({userName:'heeya',password:'123'}))
+// store.dispatch(removeUser({id: userOne.user.id }))
+// store.dispatch(editUser(userTwo.user.id, {password: '567'}))
+
+// const login = (credentials) => async (dispatch) => {
+ 
+//     try {
+//       // Replace with your login API call
+//       const fakeLoginApi = 'http://localhost:9000/users';
+//       const response = await fakeLoginApi(credentials);
+//       dispatch(loginSuccess(response.user));
+//     } catch (error) {
+//       dispatch(loginFailure(error.message));
+//     }
+//   };
+const loginUser = () => {
+    return async (dispatch) => {
+    //    console.log(credentials)
+        try {
+            const userName = "Neema";
+            const response = await fetch(`http://localhost:9000/users/${userName}`);
+                // , {
+            //     method: 'GET',
+            //     // headers: { 'Content-Type': 'application/json' },
+            //     // body: JSON.response(credentials),
+            // });
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(data);
+           
+            if (response.ok) {
+                dispatch(loginSuccess(data.user));
+            } else {
+                dispatch(loginFailure(data.error));
+            }
+        } catch (error) {
+            dispatch(loginFailure(error.message));
+        }
+    };
+};
+  store.dispatch(loginUser());
 const demoState = {
     user: [{
       userName : 'Meet',
