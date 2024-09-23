9253db8e108ee2c484c422a7384b1d9f3aee6b98
Neema <patelneema93@gmail.com>
2024-09-23 16:02:30 -0400

    create auth reducer

---
+++ expensify/src/reducers/auth.jsx
@@ -0,0 +1,34 @@
+
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+    loading: false,
+    error: null,
+  };
+  
+  const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+   
+      case 'LOGIN_SUCCESS':
+        return {
+          ...state,
+          isAuthenticated: true,
+          user: action.payload,
+          loading: false,
+        };
+      case 'LOGIN_FAILURE':
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+      case 'LOGOUT':
+        return {
+          ...state,
+          isAuthenticated: false,
+          user: null,
+        };
+      default:
+        return state;
+    }
+  };
--- expensify/src/reducers/user.jsx
+++ expensify/src/reducers/user.jsx
@@ -6,5 +6,8 @@ const initialStateUser = {
     switch (action.type) {
         case 'ADD_USER':
         return { ...state, users: [...state.users, action.payload] };
+   
+    default:
+        return state;
     }
   };
