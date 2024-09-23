fb542d071d8c700252395bb7ed8d7dff87502bc3
Neema <patelneema93@gmail.com>
2024-09-23 16:37:29 -0400

    add action and reducer fetch data file

--- expensify/src/reducers/user.jsx
+++ expensify/src/reducers/user.jsx
@@ -4,6 +4,8 @@ const initialStateUser = {
   
   export default(state = initialStateUser, action) => {
     switch (action.type) {
+        case 'FETCH_USERS':
+            return { ...state, users: action.payload };
         case 'ADD_USER':
         return { ...state, users: [...state.users, action.payload] };
    
